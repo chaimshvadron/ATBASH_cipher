b42d4079a9bf43bc29d6a3652e09550ab4bf69c1
Arie <s7925al@gmail.com>
2025-05-14 17:02:45 +0300

    function PrintFinall add

--- ATBASH_cipher/Program.cs
+++ ATBASH_cipher/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ATBASH_cipher
+﻿using System.Net.Mail;
+
+namespace ATBASH_cipher
 {
     class Program
     {
         static void Main()
         {
+            PrintFinally(5, "jninmxim");
+        }
 
+        static void PrintFinally(int conter, string str)
+        {
+            string warning = "";
+            if (conter > 0 && conter <= 5)
+            {
+                warning = "WARNING";
+            }
+            else if (conter > 5 && conter < 11)
+            {
+                warning = "DANGER";
+            }
+            else if (conter >= 11 && conter < 15)
+            {
+                warning = "ULTRA ALTERT";
+            }
+            string MssageFinallY = "";
+
+            if (conter > 0)
+            {
+                MssageFinallY = $"   {warning}\n{str}\n{conter}the suspect words";
+            }
+            else
+            {
+                MssageFinallY = "No suspicious words found.";
+            }
+            Console.WriteLine(MssageFinallY);
         }
     }
 }
